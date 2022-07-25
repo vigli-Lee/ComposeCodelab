17e6f1264e8f0264d5b26f9bc506f7c4754439a6
vigli-Lee <wnsqjadl@gmail.com>
2022-07-25 23:43:16 +0900

    Android Compose Tutorial Lesson4

--- app/src/main/java/com/vigli/composecodelab/MainActivity.kt
+++ app/src/main/java/com/vigli/composecodelab/MainActivity.kt
@@ -4,22 +4,28 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -32,13 +38,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeCodelabTheme {
-                MessageCard(Message("Android", "Jetpack Compose"))
+                Conversation(conversationSample)
             }
         }
     }
 }
 
 data class Message(val author: String, val body: String)
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
 
 @Composable
 fun MessageCard(msg: Message) {
@@ -51,27 +66,83 @@ fun MessageCard(msg: Message) {
                 .clip(CircleShape)
                 .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
-        
 
-        Column {
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = msg.author,
                 color = MaterialTheme.colors.secondaryVariant,
                 style = MaterialTheme.typography.subtitle2
             )
+
             Spacer(modifier = Modifier.height(4.dp))
 
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.body2
                 )
             }
-        }    
+        }
     }
     
 }
+
+private val conversationSample = listOf(
+    Message("Android", "Jetpack Compose\nHello"),
+    Message("Android", "Jetpack Compose2\n" +
+            "Hello"),
+    Message("Android", "Jetpack Compose3\n" +
+            "Hello"),
+    Message("Android", "Jetpack Compose4\n" +
+            "Hello"),
+    Message("Android", "Jetpack Compose5\n" +
+            "Hello"),
+    Message("Android", "Jetpack Compose6\n" +
+            "Hello"),
+    Message("Android", "Jetpack Compose7\n" +
+            "Hello"),
+    Message("Android", "Jetpack Compose8\n" +
+            "Hello"),
+    Message("Android", "Jetpack Compose9\n" +
+            "Hello"),
+    Message("Android", "Jetpack Compose\nHello"),
+    Message("Android", "Jetpack Compose2\n" +
+            "Hello"),
+    Message("Android", "Jetpack Compose3\n" +
+            "Hello"),
+    Message("Android", "Jetpack Compose4\n" +
+            "Hello"),
+    Message("Android", "Jetpack Compose5\n" +
+            "Hello"),
+    Message("Android", "Jetpack Compose6\n" +
+            "Hello"),
+    Message("Android", "Jetpack Compose7\n" +
+            "Hello"),
+    Message("Android", "Jetpack Compose8\n" +
+            "Hello"),
+    Message("Android", "Jetpack Compose9\n" +
+            "Hello"),
+)
 
 @Preview(name = "Light Mode")
 @Preview(
@@ -82,9 +153,7 @@ fun MessageCard(msg: Message) {
 @Composable
 fun PreviewMessageCard() {
     ComposeCodelabTheme {
-        MessageCard(
-            msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
-        )
+        Conversation(messages = conversationSample)
     }
 }
 
